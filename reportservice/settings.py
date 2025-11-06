dbc95973b6bef0c94afe820037baebd965066fc1
nguyennhathaicomputer <nguyennhathaigithub@gmail.com>
2025-11-06 16:11:24 +0700

    edit setting file add static root

--- reportservice/settings.py
+++ reportservice/settings.py
@@ -176,6 +176,7 @@ LOGIN_URL = 'app:login'
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app/static')]
 
 # Default primary key field type
