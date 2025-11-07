c293d3a07f308f93ff0e695245f3a7f19e96332d
nguyennhathaicomputer <nguyennhathaigithub@gmail.com>
2025-11-07 22:20:28 +0700

    remove no input aug

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --no-input
+python manage.py collectstatic
 
 # Run migrations
 python manage.py migrate
