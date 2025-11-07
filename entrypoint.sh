aa1ef6c294152de7c1221b9393db415cdf316d61
nguyennhathaicomputer <nguyennhathaigithub@gmail.com>
2025-11-07 22:32:52 +0700

    edit entryponit

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 python manage.py migrate 
 
 # Collect static files
-python manage.py collectstatic --no-input
+python manage.py collectstatic
 
 # Start Django server
 python manage.py runserver 0.0.0.0:8000
